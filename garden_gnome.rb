f0753f98f328b9bb67d62eec1b1544e0bd9734f6
Christina Ng <christinasy317@gmail.com>
2018-07-29 14:11:22 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,53 +1,27 @@
 # Code your instances here
 
 class GardenGnome
-
-  def initialize(hat_color = "red")
+  
+  attr_accessor :name, :age, :allergy, :gluten_allergy, :personality, :hat_color 
+  
+  def initialize(personality = "evil", hat_color = "red")
+    @personality = personality 
     @hat_color = hat_color
-  end
-
-  def hat_color
-    @hat_color
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def age=(age)
-    @age = age
-  end
-
-  def age
-    @age
-  end
-
-  def gluten_allergy=(allergy)
-    @allergy = allergy
-  end
-
-  def gluten_allergy
-    @allergy
-  end
-
-  def personality
-    @personality = "evil"
-  end
-
-  def gnaw
-       "Gnawing on a tree!!!"
-    end
-
+    
+  end 
+  
+  def gnaw 
+    "Gnawing on a tree!!!"
+  end 
+  
   def shout
-     "GNARLY!!!"
-  end
-
-  def introduce_self
+    "GNARLY!!!"
+  end 
+  
+  def introduce_self 
     "Hello humans, my name is #{@name}, I am #{@age} years old, and you'll rue the day you crossed me!"
-  end
+  end 
+
+
 
 end
